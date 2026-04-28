ed577f397e3a3c019242b9d6e529d23093dbab99
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-04-28 23:20:28 +0100

    Create Home.jsx
    Yes

---
+++ client/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to SKYDROP</h1>
+      <p>Social + Mining + AI Platform</p>
+    </div>
+  );
+}
