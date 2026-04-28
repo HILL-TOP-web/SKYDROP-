ecaafa88991502d0fcad9feaa0e1b7015531e8f0
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-04-28 23:18:25 +0100

    Create AuthLayout.jsx
    Yes

---
+++ client/layouts/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AuthLayout({ children }) {
+  return (
+    <div className="auth-layout">
+      <div className="auth-box">
+        {children}
+      </div>
+    </div>
+  );
+}
