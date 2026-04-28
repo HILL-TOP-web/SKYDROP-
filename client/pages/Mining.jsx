be531289d321a2b7fe67d50bc78422af7f3989b0
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-04-28 23:28:34 +0100

    Create Mining.jsx
    Yes

---
+++ client/pages/Mining.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+import MiningButton from '../components/MiningButton';
+
+export default function Mining() {
+  return (
+    <MainLayout>
+      <h1>Mining Center</h1>
+
+      <MiningButton />
+    </MainLayout>
+  );
+}
