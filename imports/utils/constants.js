97a3c3f0d759aa7b069560f246707f7e59e31d50
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-05-01 23:37:24 +0100

    Create constants.js
    Yes

---
+++ imports/utils/constants.js
@@ -0,0 +1,25 @@
+export const APP_NAME = 'SKYDROP';
+
+export const SKD_PRICE_USD = 2000;
+
+export const TOTAL_SUPPLY = 21000000;
+
+export const MINING = {
+  RATE_PER_SECOND: 0.0001,
+  MAX_SESSION_HOURS: 24,
+  REFERRAL_BONUS: 0.05,
+};
+
+export const WALLET = {
+  MIN_TRANSFER: 0.001,
+  MAX_TRANSFER: 100000,
+};
+
+export const API = {
+  VERSION: 'v1',
+};
+
+export const SECURITY = {
+  MAX_LOGIN_ATTEMPTS: 5,
+  JWT_EXPIRES: '7d',
+};
